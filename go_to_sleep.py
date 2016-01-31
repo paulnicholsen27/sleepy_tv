a5f5edbdd75f7267648a3db41567ab368ff65f60
Paul Nicholsen <pnichols104@gmail.com>
2016-01-31 17:27:06 -0500

    initial commit, program works

---
+++ go_to_sleep.py
@@ -0,0 +1,18 @@
+import vol
+from time import sleep
+from subprocess import call, check_output
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--minutes_until_mute", type=int)
+a = parser.parse_args()
+minutes_until_mute = a.minutes_until_mute if a.minutes_until_mute else 30
+
+current_volume = check_output(['osascript', '-e', 'set ovol to output volume of (get volume settings)'])
+amount_to_subtract = int(current_volume) / minutes_until_mute
+
+for i in range(minutes_until_mute):
+    current_volume = int(current_volume)
+    current_volume -= amount_to_subtract
+    call(["vol", "out", str(current_volume)])
+    sleep(60)
