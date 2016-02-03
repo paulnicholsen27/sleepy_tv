d20303dd3c928a040d58bce20171cdf1c2907a5e
Paul Nicholsen <pnichols104@gmail.com>
2016-02-03 17:57:04 -0500

    made function quadratic instead of linear

--- go_to_sleep.py
+++ go_to_sleep.py
@@ -6,16 +6,17 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--minutes_until_mute", help="time (in minutes) to set the volume to mute", type=int)
 a = parser.parse_args()
-quarter_minutes_until_mute = a.minutes_until_mute * 4 if a.minutes_until_mute else 30 * 4
+minutes_until_mute = a.minutes_until_mute if a.minutes_until_mute else 30
 
 current_volume = check_output(['osascript', '-e', 'set ovol to output volume of (get volume settings)'])
-amount_to_subtract = int(current_volume) / quarter_minutes_until_mute
 
-for i in range(quarter_minutes_until_mute):
-    current_volume = int(current_volume)
-    current_volume -= amount_to_subtract
-    call(["vol", "out", str(current_volume)])
+for i in range(minutes_until_mute):
+    percent_of_volume = (-1.0/(minutes_until_mute**2))*(i**2) + 1
+    new_volume = int(float(current_volume) * percent_of_volume)
+    print "{}% of {} at minute {} is {}".format(percent_of_volume, current_volume, i, new_volume)
+
+    call(["vol", "out", str(new_volume)])
     call(["vol", "info"])
-    sleep(15)
+    sleep(60)
 
 call(["vol", "mute"]) #for the likely chance that I stupidly manually increased the volume
