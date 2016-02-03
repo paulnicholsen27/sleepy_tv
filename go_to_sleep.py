bbe028eb841c4ab23e1f30a9b703ea8b53aacf42
Paul Nicholsen <pnichols104@gmail.com>
2016-02-02 22:37:33 -0500

    changed to quarter minutes

--- go_to_sleep.py
+++ go_to_sleep.py
@@ -6,15 +6,16 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--minutes_until_mute", help="time (in minutes) to set the volume to mute", type=int)
 a = parser.parse_args()
-minutes_until_mute = a.minutes_until_mute if a.minutes_until_mute else 30
+quarter_minutes_until_mute = a.minutes_until_mute * 4 if a.minutes_until_mute else 30 * 4
 
 current_volume = check_output(['osascript', '-e', 'set ovol to output volume of (get volume settings)'])
-amount_to_subtract = int(current_volume) / minutes_until_mute
+amount_to_subtract = int(current_volume) / quarter_minutes_until_mute
 
-for i in range(minutes_until_mute):
+for i in range(quarter_minutes_until_mute):
     current_volume = int(current_volume)
     current_volume -= amount_to_subtract
     call(["vol", "out", str(current_volume)])
-    sleep(60)
+    call(["vol", "info"])
+    sleep(15)
 
-call(["vol", "mute"])
+call(["vol", "mute"]) #for the likely chance that I stupidly manually increased the volume
