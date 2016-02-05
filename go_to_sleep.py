c6e43a6317a7e0909f80137220675edac554c3b1
Paul Nicholsen <pnichols104@gmail.com>
2016-02-05 07:20:00 -0500

    provided default arguments to argparse to be less dumb

--- go_to_sleep.py
+++ go_to_sleep.py
@@ -4,19 +4,17 @@ from subprocess import call, check_output
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--delay", help="time (in minutes) until volume begins to lower", type=int)
-parser.add_argument("-m", "--minutes_until_mute", help="time (in minutes) to set the volume to mute", type=int)
+parser.add_argument("-d", "--delay", help="time (in minutes) until volume begins to lower", default=30, type=int)
+parser.add_argument("-m", "--minutes_until_mute", help="time (in minutes) to set the volume to mute", default=0, type=int)
 a = parser.parse_args()
-minutes_until_mute = a.minutes_until_mute if a.minutes_until_mute else 30
-delay = a.delay if a.delay else 0
-sleep(delay*60)
-current_volume = check_output(['osascript', '-e', 'set ovol to output volume of (get volume settings)'])
+sleep(a.delay*1)
+current_volume = float(check_output(['osascript', '-e', 'set ovol to output volume of (get volume settings)']))
 
-for i in range(minutes_until_mute):
-    percent_of_volume = (-1.0/(minutes_until_mute**2))*(i**2) + 1 #quadratic function with maximum at (0, 100%) and crossing at (minutes_until_mute, 0)
-    new_volume = int(float(current_volume) * percent_of_volume)
+for i in range(a.minutes_until_mute+1):
+    percent_of_volume = (-1.0/(a.minutes_until_mute**2))*(i**2) + 1 #quadratic function with maximum at (0, 100%) and crossing at (minutes_until_mute, 0)
+    new_volume = int(current_volume * percent_of_volume)
     call(["vol", "out", str(new_volume)])
     call(["vol", "info"])
-    sleep(60)
+    sleep(1)
 
 call(["vol", "mute"]) #accounts for rounding errors 
