1b56d6f9a96abfd41b097a1a06cb496d7703b622
Paul Nichols <pnicholsen@theatlantic.com>
2016-09-08 13:46:51 -0400

    cleanup

--- go_to_sleep.py
+++ go_to_sleep.py
@@ -7,12 +7,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     "-d", "--delay",
     help="time (in minutes) until volume begins to lower",
-    default=30,
+    default=0,
     type=int)
 parser.add_argument(
     "-m", "--minutes_until_mute",
     help="time (in minutes) to set the volume to mute",
-    default=0,
+    default=30,
     type=int)
 a = parser.parse_args()
 sleep(a.delay * 60)
@@ -20,14 +20,13 @@ current_volume = float(check_output(
     ['osascript', '-e',
         'set ovol to output volume of (get volume settings)']))
 
-for i in range(a.minutes_until_mute+1):
-    percent_of_volume = (-1.0/(a.minutes_until_mute**2))*(i**2) + 1
-    #quadratic function with maximum at (0, 100%)
-    #and crossing at (minutes_until_mute, 0)
+for i in range(a.minutes_until_mute + 1):
+    percent_of_volume = (-1.0 / (a.minutes_until_mute**2)) * (i**2) + 1
+    # quadratic function with maximum at (0, 100%)
+    # and crossing at (minutes_until_mute, 0)
     new_volume = int(current_volume * percent_of_volume)
     call(["vol", "out", str(new_volume)])
     call(["vol", "info"])
     sleep(60)
 
-call(["vol", "mute"])
-#accounts for rounding errors
+call(["vol", "mute"])  # accounts for rounding errors
