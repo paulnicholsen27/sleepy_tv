0c290188d4f85f9b9701ed848e0ce7ff32ae2f72
Paul Nicholsen <paulnicholsen27@gmail.com>
2018-03-13 09:55:37 -0400

    added default option to mute at sunset (UTC)

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ The program takes two arguments:
 **-m / --minutes_until_mute**: the length of time before the volume will completely be muted.  It is based off a quadratic formula so it will start lowering slowly and speed up as time progresses
 
 **-d / --delay**: number of minutes before the volume will begin to lower
+
+Note:  If no value is entered for minutes_until_mute, the volume will begin to lower immediately (or after the delay, if provided) and will reach zero at sunset UTC.
--- go_to_sleep.py
+++ go_to_sleep.py
@@ -1,18 +1,43 @@
-import vol
-from time import sleep
-from subprocess import call, check_output
 import argparse
+from datetime import datetime
+import ipdb
+import json
+from subprocess import call, check_output
+from time import sleep
+from urllib import urlopen
+
+import vol
+
+sunset_api_url = "https://api.sunrise-sunset.org/json?lat=38.897834&lng=-77.032992"
+sun_data = json.loads(urlopen(sunset_api_url).read())
+sunset = sun_data['results']['sunset']
+present_time = datetime.now()
+sunset_time = datetime.strptime(sunset, '%H:%M:%S %p')  # get time of sunset
+
+# convert time of sunset to today's date
+sunset_time_today = datetime(present_time.year,
+                             present_time.month,
+                             present_time.day,
+                             sunset_time.hour + 12,  # assuming sunset is always after noon
+                             sunset_time.minute,
+                             sunset_time.second)
+
+# time difference between now and sunset
+delta = sunset_time_today - present_time
+minutes_until_sunset = delta.seconds / 60.0
 
 parser = argparse.ArgumentParser()
+
 parser.add_argument(
     "-d", "--delay",
     help="time (in minutes) until volume begins to lower",
     default=0,
     type=int)
+
 parser.add_argument(
     "-m", "--minutes_until_mute",
     help="time (in minutes) to set the volume to mute",
-    default=30,
+    default=minutes_until_sunset,
     type=int)
 a = parser.parse_args()
 sleep(a.delay * 60)
@@ -20,7 +45,7 @@ starting_volume = float(check_output(
     ['osascript', '-e',
         'set ovol to output volume of (get volume settings)']))
 
-for i in range(a.minutes_until_mute + 1):
+for i in range(a.minutes_until_mute + 1): 
     percent_of_volume = (-1.0 / (a.minutes_until_mute**2)) * (i**2) + 1
     # quadratic function with maximum at (0, 100%)
     # and crossing at (minutes_until_mute, 0)
